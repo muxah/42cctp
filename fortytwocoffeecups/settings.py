b5c216a52bb38646f1c1c759c31c33dbe28a3a9e
Mikhail Akovantsev <mikhail.akovantsev@gmail.com>
2010-04-06 05:08:47 +0300

    t1: empty home page view, template implemented; view binded to an url.

--- fortytwocoffeecups/settings.py
+++ fortytwocoffeecups/settings.py
@@ -1,4 +1,7 @@
 # Django settings for fortytwocoffeecups project.
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -67,9 +70,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'fortytwocoffeecups.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
--- fortytwocoffeecups/urls.py
+++ fortytwocoffeecups/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from bcard.views import home
+
 
 urlpatterns = patterns('',
+    (r'^$', home),
     # Example:
     # (r'^fortytwocoffeecups/', include('fortytwocoffeecups.foo.urls')),
 
