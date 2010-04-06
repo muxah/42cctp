d98d3832c071ac598afe8bb8589a838f325315d0
Mikhail Akovantsev <mikhail.akovantsev@gmail.com>
2010-04-06 05:03:06 +0300

    basic tests for home page view, url and template.

--- fortytwocoffeecups/bcard/tests.py
+++ fortytwocoffeecups/bcard/tests.py
@@ -1,23 +1,21 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
 __test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+>>> from bcard.views import home
+>>> home()
+Traceback (most recent call last):
+...
+TypeError: home() takes exactly 1 ...
 
->>> 1 + 1 == 2
+>>> type(home('1'))
+<class 'django.http.HttpResponse'>
+
+>>> from settings import TEMPLATE_DIRS
+>>> bool(TEMPLATE_DIRS)
 True
-"""}
 
+>>> from django.test.client import Client
+>>> c = Client()
+>>> response = c.get('/')
+>>> response.status_code
+200
+
+"""}
