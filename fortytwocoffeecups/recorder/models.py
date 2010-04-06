a167c6746d99403a15b097097d70ec118fb96a56
Mikhail Akovantsev <mikhail.akovantsev@gmail.com>
2010-04-06 08:36:05 +0300

    t3: added tests for RecordedRequest model and blank model itself

--- fortytwocoffeecups/recorder/models.py
+++ fortytwocoffeecups/recorder/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class RecordedRequest(models.Model):
+    """
+    Model purposed for storing each http request by RecordingMiddleware.
+
+    # It is possible to store new request:
+    >>> dummy_request = {'dummy':'request'}
+    >>> rr = RecordedRequest(request=dummy_request)
+    >>> rr.save()
+
+    # And retreive it back looking exactly the same:
+    >>> rr.get_request() == request
+    True
+
+    """
