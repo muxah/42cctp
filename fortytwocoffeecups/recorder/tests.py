4bbfd3b1b751d7412c5639b38b867179236f83f5
Mikhail Akovantsev <mikhail.akovantsev@gmail.com>
2010-04-06 08:17:25 +0300

    t3: created blank recorder app; added tests for middleware class mock

---
+++ fortytwocoffeecups/recorder/tests.py
@@ -0,0 +1,22 @@
+__test__ = {"doctest": """
+>>> from recorder.middleware import RecordingMiddleware as RM
+>>> hasattr(RM, 'process_request')
+True
+
+>>> RM.process_request()
+Traceback (most recent call last):
+...
+TypeError: unbound method process_request() must be called with a instance ...
+
+>>> RM().process_request()
+Traceback (most recent call last):
+...
+TypeError: process_request() takes exactly 2 ...
+
+>>> RM().process_request('dummy request')
+
+>>> from settings import MIDDLEWARE_CLASSES as MC
+>>> 'recorder.middleware.RecordingMiddleware' in MC
+True
+
+"""}
