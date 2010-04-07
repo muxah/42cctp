0f8ed578c29dc1fb160cb67201d68e4aa2ed2759
Mikhail Akovantsev <mikhail.akovantsev@gmail.com>
2010-04-07 23:50:59 +0300

    t5: tests refactoring: got rid of the try-except; + form behavior tests: prefilled, redirects, validates, has csrf token.

--- fortytwocoffeecups/bcard/tests.py
+++ fortytwocoffeecups/bcard/tests.py
@@ -2,80 +2,52 @@ from django.test import TestCase
 from django.test.client import Client
 import django
 
+from models import BusinessCard
+
 BCARD_FIELDS = ('first_name', 'last_name', 'email', 'description',)
 
 
 class TemlateContextProcessorsTest(TestCase):
 
     def test_existence_and_format(self):
-        try:
-            from bcard.context_processors import settings
-        except ImportError as e:
-            self.fail(e)
-        else:
-            result = settings('dummy')
-            self.assertTrue(isinstance(result, dict))
-            self.assertRaises(TypeError, settings)
+        from bcard.context_processors import settings
+        result = settings('dummy')
+        self.assertTrue(isinstance(result, dict))
+        self.assertRaises(TypeError, settings)
 
     def test_integration(self):
-        try:
-            from settings import TEMPLATE_CONTEXT_PROCESSORS
-        except ImportError as e:
-            self.fail(e)
-        else:
-            cpf = 'bcard.context_processors.settings'
-            self.assertTrue(cpf in TEMPLATE_CONTEXT_PROCESSORS)
+        from settings import TEMPLATE_CONTEXT_PROCESSORS
+        cpf = 'bcard.context_processors.settings'
+        self.assertTrue(cpf in TEMPLATE_CONTEXT_PROCESSORS)
 
 
 class EditBCFormTest(TestCase):
 
     def test_existence_and_format(self):
-        try:
-            from bcard.forms import EditBusinessCardForm
-        except ImportError as e:
-            self.fail(e)
-        else:
-            cls = django.forms.models.ModelFormMetaclass
-            self.assertTrue(isinstance(EditBusinessCardForm, cls))
-            fields = [f.name for f in EditBusinessCardForm().visible_fields()]
+        from bcard.forms import EditBusinessCardForm
+        cls = django.forms.models.ModelFormMetaclass
+        self.assertTrue(isinstance(EditBusinessCardForm, cls))
+        fields = [f.name for f in EditBusinessCardForm().visible_fields()]
 
-            for a in BCARD_FIELDS:
-                self.assertTrue(a in fields)
+        for a in BCARD_FIELDS:
+            self.assertTrue(a in fields)
 
     def test_integration(self):
-        try:
-            from views import EditBusinessCardForm
-        except ImportError as e:
-            self.fail(e)
+        from views import EditBusinessCardForm
 
 
 class EditBCViewTest(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.url = '/edit/'
 
     def test_existance_and_format(self):
-        try:
-            from views import edit
-        except ImportError as e:
-            self.fail(e)
-        else:
-            self.assertRaises(TypeError, edit)
+        from views import edit
 
-            try:
-                response = edit('dummy_request')
-            except django.template.TemplateDoesNotExist as e:
-                self.fail('TemplateDoesNotExist: %s' % e)
-            else:
-                cls = django.http.HttpResponse
-                self.assertTrue(isinstance(response, cls))
-
-        response = self.client.get('/edit/', {})
-
-        try:
-            response.context['form']
-        except KeyError:
-            self.fail('context has no form key')
+        self.assertRaises(TypeError, edit)
+        response = self.client.get(self.url)
+        response.context['form']
 
         ids = ['id_%s' % a for a in BCARD_FIELDS]
         for id in ids:
@@ -84,13 +56,36 @@ class EditBCViewTest(TestCase):
         self.assertTrue('method="post"' in response.content)
 
     def test_integration(self):
-        try:
-            from urls import edit
-        except ImportError as e:
-            self.fail(e)
+        from urls import edit
 
-        response = self.client.get('/edit/')
+        response = self.client.get(self.url)
         self.failUnlessEqual(response.status_code, 200)
+        person = BusinessCard.objects.get(pk=1)
+        for a in BCARD_FIELDS:
+            self.assertTrue(getattr(person, a) in response.content)
+
+    def test_functionality(self):
+        required = {'first_name': 'Mate', 'last_name': 'Rolling'}
+        optional = {'email': 'mate@rolling.com', 'description': 'Nothing to c.'}
+        all = required
+        all.update(optional)
+        empty = dict([(k, '') for k in BCARD_FIELDS])
+        incorrect = all.copy()
+        incorrect['email'] = 'broken_email'
+        default = {}
+
+        for d in (empty, optional, incorrect):
+            response = self.client.post(self.url, d)
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue('class="errorlist"' in response.content)
+            self.assertTrue("name='csrfmiddlewaretoken'" in response.content)
+            self.assertTrue('edit.html' in [t.name for t in response.template])
+
+        for d in (default, required, all):
+            response = self.client.post(self.url, all, follow=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(('http://testserver/', 302) in response.redirect_chain)
+            self.assertTrue('home.html' in [t.name for t in response.template])
 
 
 __test__ = {"doctest": """
