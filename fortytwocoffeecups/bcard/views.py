b8f1c2512cb669b83416c17ffd78ae22db84d8a5
Mikhail Akovantsev <mikhail.akovantsev@gmail.com>
2010-04-08 00:13:44 +0300

    t5: form functionality implementation (all except saving: validation, redirect, csrf, prepopulation).

--- fortytwocoffeecups/bcard/views.py
+++ fortytwocoffeecups/bcard/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+
 from models import BusinessCard
 from forms import EditBusinessCardForm
 
@@ -13,5 +16,14 @@ def home(request):
 
 
 def edit(request):
-    form = EditBusinessCardForm()
-    return render_to_response('edit.html', {'form': form})
+    person = BusinessCard.objects.get(pk=1)
+    form = EditBusinessCardForm(instance=person)
+
+    if request.method == 'POST':
+        form = EditBusinessCardForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/')
+
+    return render_to_response('edit.html',
+                             {'form': form},
+                             context_instance=RequestContext(request))
