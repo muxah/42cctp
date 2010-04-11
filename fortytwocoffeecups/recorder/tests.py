8d1eec653ab2f5703182a59ad44f298d0cdd6920
Mikhail Akovantsev <mikhail.akovantsev@gmail.com>
2010-04-11 21:34:40 +0300

    t10: integration tests, tests on storing signals

--- fortytwocoffeecups/recorder/tests.py
+++ fortytwocoffeecups/recorder/tests.py
@@ -23,10 +23,29 @@ class RequestIsStoredTest(TestCase):
     def test_existence_and_format(self):
         from recorder.middleware import RecordingMiddleware as RM
         self.assertTrue(hasattr(RM, 'process_request'))
-        self.assertRaises(TypeError, RM.edit)
-        self.assertRaises(TypeError, RM().edit)
+        self.assertRaises(TypeError, RM.process_request)
+        self.assertRaises(TypeError, RM().process_request)
 
     def test_integration(self):
         from models import RecordedRequest
         from settings import MIDDLEWARE_CLASSES as MC
         self.assertTrue('recorder.middleware.RecordingMiddleware' in MC)
+
+
+class SignalIsStoredTest(TestCase):
+
+    def test_init_is_stored(self):
+        from models import RecordedRequest as RR
+        from models import RecordedSignal as RS
+
+        before_init = len(RS.objects.all())
+        obj = RR(request=repr({'dummy': 'request'}))
+        after_init = len(RS.objects.all())
+        obj.save()
+        after_save = len(RS.objects.all())
+        obj.delete()
+        after_delete = len(RS.objects.all())
+
+        self.assertEqual(after_init - before_init, 1)
+        self.assertEqual(after_save - after_init, 1)
+        self.assertEqual(after_delete - after_save, 1)
