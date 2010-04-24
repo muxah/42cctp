a308f9298a919b6e86677cf2270fed818fab42bd
Mikhail Akovantsev <mikhail.akovantsev@gmail.com>
2010-04-24 04:40:16 +0300

    t12: bcard/forms.py: blank lines, line length

--- fortytwocoffeecups/bcard/forms.py
+++ fortytwocoffeecups/bcard/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 from models import BusinessCard
-from django.forms.extras.widgets import SelectDateWidget
 from django.contrib.admin.widgets import AdminDateWidget
+
 
 class EditBusinessCardForm(forms.ModelForm):
     birth_date = forms.DateField(widget=AdminDateWidget)
 
     class Meta:
         model = BusinessCard
-        fields = ('first_name', 'last_name', 'email', 'description', 'birth_date',)
+        fields = (
+                'first_name',
+                'last_name',
+                'email',
+                'description',
+                'birth_date',
+        )
         fields = fields[::-1]
